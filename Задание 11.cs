32cf62d3b6d4d17e74f1a58dc3e0bb2491da443c
Анастасия Меньшикова <menschickova2011@mail.ru>
2022-03-12 15:04:37 +0300

    Добавьте файлы проекта.

---
+++ Задание 11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace Задание_11
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Программа решает уравнени 0 = kx + b");
+            Console.WriteLine("Вещественные числа вводятся через (,) ");
+            Console.WriteLine("Введите k :");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b : ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Equation equation = new Equation(k,b);
+            double root = equation.Root();
+            Console.WriteLine("Ответ x : {0}", root);
+            Console.ReadKey();
+        }
+    }
+}
+
+
